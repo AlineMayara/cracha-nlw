0d61e21cf52deee25c25cfc4a883def35b14661e
Aline Mayara <101646477+AlineMayara@users.noreply.github.com>
2022-05-02 09:51:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: 'alinemayara',
+  youtube: 'alinemayara',
+  facebook: 'alinemayara',
+  instagram: 'alinemayara_s',
+  twitter: 'alinemayara_s'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/$
+    {LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      UserName.textContent = data.name
+      UserBio.textContent = data.bio
+      UserLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
